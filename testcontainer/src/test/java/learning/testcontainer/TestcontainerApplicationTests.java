95df5eac4a82e09bb59bfb2db044c7a50263a780
seonghoo1217 <lsh6451217@gmail.com>
2024-08-04 13:22:54 +0900

    chore: TestContainer Dependency 추가

---
+++ testcontainer/src/test/java/learning/testcontainer/TestcontainerApplicationTests.java
@@ -0,0 +1,13 @@
+package learning.testcontainer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestcontainerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
